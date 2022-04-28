df2a0386db28ecb32c709b6fae55488599fe5996
Theau Antoine <theau.antoine@nexten.io>
2022-04-28 13:07:24 +0200

    test yarn tsc

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,12 +1,10 @@
-import type { NextPage } from 'next';
-import Head from 'next/head';
-import Image from 'next/image';
-import styles from '../styles/Home.module.css';
+import type { NextPage } from "next";
+import styles from "../styles/Home.module.css";
 
 const Home: NextPage = () => {
     return (
         <div className={styles.container}>
-            <h1 className='text-3xl font-bold'>Hello world!</h1>
+            <h1 className="text-3xl font-bold">Hello world!</h1>
         </div>
     );
 };
